3542c967921be6d9cd10a1e46bb2b6621cad2540
0000sir <0000sir@gmail.com>
2016-09-26 16:15:11 +0800

    downscale image size

--- bin/convert.sh
+++ bin/convert.sh
@@ -4,4 +4,4 @@
 #th ~/neural-style/neural_style.lua -style_image $1 -content_image $2 -output_image $3 -gpu 0 -backend clnn $4 -num_iterations 500 -image_size 512
 
 cd ~/texture_nets/texture_nets-master/
-th test.lua -cpu -image_size 1024 -input_image $2 -save_path $3 -model_t7 $1 -cpu
+th test.lua -cpu -image_size 512 -input_image $2 -save_path $3 -model_t7 $1 -cpu
